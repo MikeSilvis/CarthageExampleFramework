69bf48c38bac542cf1f673df7eb2998dc9aa1e5e
Mike Silvis <msilvis@squareup.com>
2020-04-11 12:38:00 -0400

    Initial Commit

---
+++ CarthageExampleFramework/Example.swift
@@ -0,0 +1,13 @@
+//
+//  Example.swift
+//  CarthageExampleFramework
+//
+//  Created by Mike Silvis on 4/11/20.
+//  Copyright © 2020 Yab. All rights reserved.
+//
+
+import Foundation
+
+class Example {
+    
+}
