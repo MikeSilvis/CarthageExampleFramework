d6ee7c6e49bf7deec7e146786e6462430f8776d3
Mike Silvis <msilvis@squareup.com>
2020-04-11 12:38:00 -0400

    Initial Commit

---
+++ CarthageExampleFramework/CarthageExampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  CarthageExampleFramework.h
+//  CarthageExampleFramework
+//
+//  Created by Mike Silvis on 4/11/20.
+//  Copyright © 2020 Yab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarthageExampleFramework.
+FOUNDATION_EXPORT double CarthageExampleFrameworkVersionNumber;
+
+//! Project version string for CarthageExampleFramework.
+FOUNDATION_EXPORT const unsigned char CarthageExampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageExampleFramework/PublicHeader.h>
+
+
